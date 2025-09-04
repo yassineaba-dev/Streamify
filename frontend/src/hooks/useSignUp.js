c621d18c14fe54c22a342c3371eafaeb8a71de1b
yassineaba-dev <yassineaba.dev@gmail.com>
2025-09-04 09:20:18 +0100

    first commit

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -1,14 +1,21 @@
+// useSignUp.js
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { signup } from "../lib/api";
 
 const useSignUp = () => {
   const queryClient = useQueryClient();
 
-  const { mutate, isPending, error } = useMutation({
+  const { mutate, isLoading, error } = useMutation({
     mutationFn: signup,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+    onSuccess: (data) => {
+      if (data?.token) {
+        localStorage.setItem("token", data.token);
+      }
+      // localStorage.setItem("hasSession", "1"); // if cookie-based
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
   });
 
-  return { isPending, error, signupMutation: mutate };
+  return { isLoading, error, signupMutation: mutate };
 };
 export default useSignUp;
