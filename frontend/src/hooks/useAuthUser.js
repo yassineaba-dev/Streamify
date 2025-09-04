815838f4ba0fb04a57d8fd9b76cb60b3923718e4
yassineaba-dev <yassineaba.dev@gmail.com>
2025-09-04 09:37:31 +0100

    first commit

--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -1,20 +1,13 @@
-// src/hooks/useAuthUser.js
 import { useQuery } from "@tanstack/react-query";
 import { getAuthUser } from "../lib/api";
 
-export default function useAuthUser() {
-  const token =
-    typeof window !== "undefined" ? localStorage.getItem("token") : null;
-
+const useAuthUser = () => {
   const authUser = useQuery({
     queryKey: ["authUser"],
     queryFn: getAuthUser,
-    enabled: !!token,
-    retry: false,
-    refetchOnWindowFocus: false,
-    staleTime: 1000 * 60 * 5,
-    select: (data) => data?.user ?? null,
+    retry: false, // auth check
   });
 
-  return { isLoading: authUser.isLoading, authUser: authUser.data ?? null };
-}
+  return { isLoading: authUser.isLoading, authUser: authUser.data?.user };
+};
+export default useAuthUser;
